2e52e594a82f83b4f4d8cd104ed72e75a5f0b1f6
Mark Boom <mark.boom@antagonist.nl>
2025-12-22 23:28:44 +0100

    Update the tracking API

--- src/py_mixpanel/tracking.py
+++ src/py_mixpanel/tracking.py
@@ -5,53 +5,49 @@ import mixpanel
 
 
 class Tracking:
-    def __init__(self, mixpanel_token: str, feature_flag: bool = True):
+    def __init__(
+        self,
+        mixpanel_token: str,
+        feature_flag: bool = True,  # deprecated @feature-flag-param-deprecated
+        enable_tracking: bool = True,
+        api_host: str = "api-eu.mixpanel.com",
+    ):
         """
         Initialize an instance of the Mixpanel tracking class.
 
-        Provide a valid `mixpanel_token`.
+        Provide a valid `mixpanel_token`. Control the Mixpanel API
+        host that is being used using `api_host`.
+
+        Tracking is enabled by default, but can be disabled by passing
+        `enable_tracking=False`.
+
+        The `feature_flag` parameter is DEPRECATED and will be removed
+        in a future version. @feature-flag-param-deprecated
         """
         self.mixpanel = mixpanel.Mixpanel(
             mixpanel_token,
-            consumer=mixpanel.Consumer(api_host="api-eu.mixpanel.com"),
+            consumer=mixpanel.Consumer(api_host=api_host),
         )
-        self.feature_flag = feature_flag
+        self.enable_tracking = feature_flag and enable_tracking
 
     def track(self, user_id, event: str, properties: t.Dict[str, t.Any]) -> None:
         """
-        Track an event in Mixpanel.
-
-        Args:
-            event: Event name
-            properties: Event properties dictionary
+        Track an event in Mixpanel. Anonymize the user ID.
         """
-        if not self.feature_flag:
+        if not self.enable_tracking:
             return
-        self.mixpanel.track(self.hash(user_id), event, properties)
+        self.mixpanel.track(self.hash_user_id(user_id), event, properties)
 
     def set_user_property(self, user_id: str, property_name: str, value: t.Any) -> None:
         """
-        Set a user property in Mixpanel.
-
-        Args:
-            user_id: User ID
-            property_name: Property name
-            value: Property value
+        Set a user property in Mixpanel. Anonymize the user ID.
         """
-        if not self.feature_flag:
+        if not self.enable_tracking:
             return
-        self.mixpanel.people_set(user_id, {property_name: value})
+        self.mixpanel.people_set(self.hash_user_id(user_id), {property_name: value})
 
-    def hash(self, value: str) -> str:
+    def hash_user_id(self, user_id: str) -> str:
         """
-        Hash a value using SHA3-224.
-
-        Args:
-            value: Value to hash
-
-        Returns:
-            Hashed string
+        Hash a user ID using SHA3-224; obscuring any personal information.
         """
-        if not self.feature_flag:
-            return ''
-        return hashlib.sha224(value.encode("utf-8")).hexdigest()
+        return hashlib.sha224(user_id.encode("utf-8")).hexdigest()
--- tests/py_mixpanel/tracking.py
+++ tests/py_mixpanel/tracking.py
@@ -1,4 +1,3 @@
-import pytest
 from unittest.mock import patch, Mock
 
 from py_mixpanel import Tracking
@@ -12,7 +11,7 @@ def test_initialization(consumer_mock: Mock, mixpanel_mock: Mock) -> None:
     consumer_mock.return_value = consumer_instance
     mixpanel_mock.return_value = mixpanel_instance
 
-    tracker = Tracking("abc", feature_flag=False)
+    tracker = Tracking("abc", enable_tracking=False)
 
     consumer_mock.assert_called_once_with(api_host="api-eu.mixpanel.com")
     mixpanel_mock.assert_called_once_with(
@@ -20,39 +19,75 @@ def test_initialization(consumer_mock: Mock, mixpanel_mock: Mock) -> None:
         consumer=consumer_instance,
     )
     assert tracker.mixpanel == mixpanel_instance
-    assert not tracker.feature_flag
+    assert not tracker.enable_tracking
+
+
+@patch("mixpanel.Consumer")
+def test_different_api_host(consumer_mock: Mock) -> None:
+    Tracking("abc", api_host="my-custom-host.local")
+    consumer_mock.assert_called_once_with(api_host="my-custom-host.local")
 
 
 @patch("py_mixpanel.tracking.hashlib.sha224")
 @patch("mixpanel.Mixpanel")
-def test_feature_flag_true(mixpanel_mock: Mock, hashlib_mock: Mock) -> None:
+def test_enable_tracking_true(mixpanel_mock: Mock, hashlib_mock: Mock) -> None:
     mixpanel_instance = Mock()
     mixpanel_mock.return_value = mixpanel_instance
 
-    tracker = Tracking("abc", feature_flag=True)
+    tracker = Tracking("abc", enable_tracking=True)
     tracker.track("abc", "def", {})
     tracker.set_user_property("abc", "def", "xyz")
-    tracker.hash("xxx")
+    tracker.hash_user_id("xxx")
 
     assert mixpanel_instance.track.call_count == 1
     assert mixpanel_instance.people_set.call_count == 1
     assert hashlib_mock.call_count > 1
 
 
+@patch("mixpanel.Mixpanel")
+def test_enable_tracking_false(mixpanel_mock: Mock) -> None:
+    mixpanel_instance = Mock()
+    mixpanel_mock.return_value = mixpanel_instance
+
+    tracker = Tracking("abc", enable_tracking=False)
+    tracker.track("abc", "def", {})
+    tracker.set_user_property("abc", "def", "xyz")
+
+    assert mixpanel_instance.track.call_count == 0
+    assert mixpanel_instance.people_set.call_count == 0
+
+
 @patch("py_mixpanel.tracking.hashlib.sha224")
 @patch("mixpanel.Mixpanel")
-def test_feature_flag_false(mixpanel_mock: Mock, hashlib_mock: Mock) -> None:
+def test_deprecated_feature_flag_parameter_enable_tracking(
+    mixpanel_mock: Mock, hashlib_mock: Mock
+) -> None:
+    # @feature-flag-param-deprecated
+    mixpanel_instance = Mock()
+    mixpanel_mock.return_value = mixpanel_instance
+
+    tracker = Tracking("abc", feature_flag=True)
+    tracker.track("abc", "def", {})
+    tracker.set_user_property("abc", "def", "xyz")
+    tracker.hash_user_id("xxx")
+
+    assert mixpanel_instance.track.call_count == 1
+    assert mixpanel_instance.people_set.call_count == 1
+    assert hashlib_mock.call_count > 1
+
+
+@patch("mixpanel.Mixpanel")
+def test_deprecated_feature_flag_parameter_disabled(mixpanel_mock: Mock) -> None:
+    # @feature-flag-param-deprecated
     mixpanel_instance = Mock()
     mixpanel_mock.return_value = mixpanel_instance
 
     tracker = Tracking("abc", feature_flag=False)
     tracker.track("abc", "def", {})
     tracker.set_user_property("abc", "def", "xyz")
-    tracker.hash("xxx")
 
     assert mixpanel_instance.track.call_count == 0
     assert mixpanel_instance.people_set.call_count == 0
-    assert hashlib_mock.call_count == 0
 
 
 @patch("mixpanel.Mixpanel")
@@ -60,7 +95,7 @@ def test_tracking(mixpanel_mock: Mock) -> None:
     mixpanel_instance = Mock()
     mixpanel_mock.return_value = mixpanel_instance
 
-    tracker = Tracking("abc", feature_flag=True)
+    tracker = Tracking("abc", enable_tracking=True)
     tracker.track("mark", "some_event", {"a": 1})
 
     mixpanel_instance.track.assert_called_once_with(
@@ -71,13 +106,27 @@ def test_tracking(mixpanel_mock: Mock) -> None:
 
 
 @patch("mixpanel.Mixpanel")
-def test_hashing(mixpanel_mock: Mock) -> None:
+def test_set_user_property(mixpanel_mock: Mock) -> None:
     mixpanel_instance = Mock()
     mixpanel_mock.return_value = mixpanel_instance
 
-    tracker = Tracking("abc", feature_flag=True)
+    tracker = Tracking("abc", enable_tracking=True)
+    tracker.set_user_property("mark", "some_property", "some_value")
+
+    mixpanel_instance.people_set.assert_called_once_with(
+        "b9e09e7675f9103783963b8cafa4992c7430ff1e21dd490ef8972cff",
+        {"some_property": "some_value"},
+    )
+
+
+@patch("mixpanel.Mixpanel")
+def test_hashing_user_id(mixpanel_mock: Mock) -> None:
+    mixpanel_instance = Mock()
+    mixpanel_mock.return_value = mixpanel_instance
+
+    tracker = Tracking("abc", enable_tracking=True)
 
     assert (
-        tracker.hash("Hey there! ✨")
+        tracker.hash_user_id("Hey there! ✨")
         == "cbb191a99676855af0093cb88834e86d61f941a4964ebabd357c77c0"
     )
